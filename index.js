174a537791a8f9a136e43a7c46641b848b49bc8a
William <wgatson@ufl.edu>
2024-02-09 05:30:11 -0500

    Final

--- index.js
+++ index.js
@@ -1,90 +1,84 @@
 var words = [
-  'bananas',
-  'grapes',
-  'carousel',
-  'milkshake',
-  'javascript',
-  'limousine',
-  'chocolate',
-  'programming',
-  'meatloaf',
-  'ukulele',
-  'mango'
-]
+	"bananas",
+	"grapes",
+	"carousel",
+	"milkshake",
+	"javascript",
+	"limousine",
+	"chocolate",
+	"programming",
+	"meatloaf",
+	"ukulele",
+	"mango",
+];
 
- function loadFunction() {
-  remainingTries = 10;
-  incorrectEl.textContent = ""
-  remainingEl.textContent = remainingTries;
-  newWordGuess = words[Math.floor(Math.random() * words.length)];
-  var underScore = "";
-  incorrectArray = [];
-  correctArray = [];
+function loadFunction() {
+	remainingTries = 10;
+	incorrectEl.textContent = "";
+	remainingEl.textContent = remainingTries;
+	newWordGuess = words[Math.floor(Math.random() * words.length)];
+	var underScore = "";
+	incorrectArray = [];
+	correctArray = [];
 
-  for (var i = 0; i < newWordGuess.length; i++) {
-    underScore += "_";
+	for (var i = 0; i < newWordGuess.length; i++) {
+		underScore += "_";
+	}
 
-  }
-
-  wordGuess.textContent = underScore;
-
+	wordGuess.textContent = underScore;
 }
-var incorrect
-var wins = 0
-var losses = 0
-var winsEl = document.getElementById('wins')
-var lossesEl = document.getElementById('losses')
-var remainingTries = 10
-var previousEl = document.getElementById('previous-word')
-var incorrectEl = document.getElementById('incorrect-letters')
-var remainingEl = document.getElementById('remaining-guesses')
-var correctArray = []
-var incorrectArray = []
-var wordGuess = document.getElementById('word-to-guess');
-var newWordGuess;
-loadFunction()
 
+var words;
+var incorrect;
+var wins = 0;
+var losses = 0;
+var winsEl = document.getElementById("wins");
+var lossesEl = document.getElementById("losses");
+var remainingTries = 10;
+var previousEl = document.getElementById("previous-word");
+var incorrectEl = document.getElementById("incorrect-letters");
+var remainingEl = document.getElementById("remaining-guesses");
+var correctArray = [];
+var incorrectArray = [];
+var wordGuess = document.getElementById("word-to-guess");
+var newWordGuess;
+
+loadFunction();
 
 document.body.onkeyup = function (e) {
-  var key = e.key.toLowerCase()
-  console.log(e.key)
+	var key = e.key.toLowerCase();
+	console.log(e.key);
 
-  if (newWordGuess.includes(key)) {
-	var underScore = "";
-	correctArray.push(key);
-	for (var i = 0; i < newWordGuess.length; i++) {
-
-
-		if (correctArray.includes(newWordGuess[i])) {
-			underScore += newWordGuess[i];
+	if (newWordGuess.includes(key)) {
+		var underScore = "";
+		correctArray.push(key);
+		for (var i = 0; i < newWordGuess.length; i++) {
+			if (correctArray.includes(newWordGuess[i])) {
+				underScore += newWordGuess[i];
+			} else {
+				underScore += "_";
+			}
 		}
-		else {
-			underScore += "_";
-		}
+
+		wordGuess.textContent = underScore;
+	} else if (!incorrectArray.includes(key) && !newWordGuess.includes(key)) {
+		incorrectArray.push(key);
+		remainingTries--;
+		remainingEl.textContent = remainingTries;
+		incorrectEl.textContent = incorrectArray;
 	}
-	
 
-    wordGuess.textContent = underScore;
-  } else if(!incorrectArray.includes(key) && !newWordGuess.includes(key)) {
-    incorrectArray.push(key);
-    remainingTries--;
-    remainingEl.textContent = remainingTries;
-    incorrectEl.textContent = incorrectArray;
-  }
+	if (wordGuess.textContent === newWordGuess) {
+		wins++;
+		winsEl.textContent = wins;
+		previousEl.textContent = newWordGuess;
 
-  if(wordGuess.textContent === newWordGuess) {
-    wins++;
-    winsEl.textContent = wins;
-    previousEl.textContent = newWordGuess;
-  
-  loadFunction()
-  
-  }  else if(remainingTries <= 0) {
-    losses++;
-    lossesEl.textContent = losses;
-    previousEl.textContent = newWordGuess;
-  
-  loadFunction()
-   
-  }
-}
+		loadFunction();
+	} else if (remainingTries <= 0) {
+		losses++;
+		lossesEl.textContent = losses;
+		previousEl.textContent = newWordGuess;
+
+		loadFunction();
+	}
+};
